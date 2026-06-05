8dbfa1c625dd3a52287756e879d85002a5510038
Jvella94 <jonathanvella07@gmail.com>
2026-06-05 13:17:24 +0200

    1.3.7
    Fixed k2 recipe ingredients.

--- prototypes/krastorio/processing-recipes.lua
+++ prototypes/krastorio/processing-recipes.lua
@@ -1,68 +1,68 @@
-local k2_processing_recipes = {{
+local k2_processing_recipes = { {
     name_root = "iron-ore-processing-k2",
     icon = "__Krastorio2Assets__/icons/items/iron-plate.png",
     order_name = "a[smelting]-a[iron-plate]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
         name = "iron-ore",
         amount = 15
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
         name = "iron-plate",
         amount_min = 7,
         amount_max = 8
-    }}
+    } }
 }, {
     name_root = "copper-ore-processing-k2",
     icon = "__Krastorio2Assets__/icons/items/copper-plate.png",
     order_name = "a[smelting]-b[copper-plate]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
         name = "copper-ore",
         amount = 15
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
         name = "copper-plate",
         amount_min = 7,
         amount_max = 8
-    }}
+    } }
 }, {
     name_root = "stone-to-stone-brick-k2",
     icon = "__base__/graphics/icons/stone-brick.png",
     order_name = "aa[stone-brick]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
         name = "stone",
         amount = 15
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
         name = "stone-brick",
         amount_min = 7,
         amount_max = 8
-    }}
+    } }
 }, {
     name_root = "sand-to-glass-k2",
     icon = "__Krastorio2Assets__/icons/items/glass.png",
     order_name = "ccc[glass]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
-        name = "sand",
+        name = krastorio2_found and "kr-sand" or "sand",
         amount = 15
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
-        name = "glass",
+        name = krastorio2_found and "kr-glass" or "glass",
         amount_min = 7,
         amount_max = 8
-    }}
+    } }
 }, {
     name_root = "wood-to-coke-k2",
     icon = "__Krastorio2Assets__/icons/items/coke.png",
     order_name = "ccc[glass]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
         name = "wood",
         amount = 15
@@ -70,17 +70,17 @@ local k2_processing_recipes = {{
         type = "item",
         name = "coal",
         amount = 9
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
         name = "kr-coke",
         amount = 15
-    }}
+    } }
 }, {
     name_root = "iron-plate-to-steel-k2",
     icon = "__Krastorio2Assets__/icons/items/steel-plate.png",
     order_name = "a[smelting]-c[steel-plate]k2",
-    ingredients = {{
+    ingredients = { {
         type = "item",
         name = "iron-plate",
         amount = 15
@@ -88,14 +88,14 @@ local k2_processing_recipes = {{
         type = "item",
         name = "kr-coke",
         amount = 3
-    }},
-    results = {{
+    } },
+    results = { {
         type = "item",
         name = "steel-plate",
         amount_min = 7,
         amount_max = 8
-    }}
-}}
+    } }
+} }
 debugLog("Adding Krastorio2 processing recipes")
 for _, recipe in ipairs(k2_processing_recipes) do
     table.insert(processing_recipes, recipe)
